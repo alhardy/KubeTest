04bdb2a3c414c28c15b1846be925b8b2f442f4f7
Allan <al_hardy@live.com.au>
2019-07-19 22:59:51 +1000

    load secrets from keyvault

--- src/KubeTest/Startup.cs
+++ src/KubeTest/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace KubeTest
@@ -21,6 +22,9 @@ namespace KubeTest
 
             app.Run(async (context) =>
             {
+                var configuration = context.RequestServices.GetRequiredService<IConfiguration>();
+                var secretKey = configuration.GetValue<string>("SecretKey");
+
                 if (context.Request.Path == "/up")
                 {
                     if (DateTime.UtcNow.Minute % 2 == 0)
@@ -35,7 +39,7 @@ namespace KubeTest
                 }
                 else
                 {
-                    await context.Response.WriteAsync($"Hello World from {Environment.MachineName}");
+                    await context.Response.WriteAsync($"Hello World from {Environment.MachineName}. Secret Key: {secretKey}");
                 }
             });
         }
